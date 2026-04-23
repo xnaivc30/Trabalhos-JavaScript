bd952d390ac2d1837bc53106d081e9f15ec9d8d2
xnaivc30 <tricknick30@gmail.com>
2026-04-23 17:21:20 -0300

    Add files via upload

---
+++ aulajs/script.js
@@ -0,0 +1,34 @@
+let nome = "Nicole";
+
+alert(nome);
+
+let seven = 7;
+let sixty = 60;
+nmr = seven + sixty;
+alert(nmr);
+
+function caixinha ( ){
+    alert("Hello, world! :D");
+}
+
+function caixinha2 ( ){
+    let texto = document.getElementById("textinho");
+    texto.textContent = "Mudoou! :D";
+}
+
+function caixinha3 ( ){
+    let cor = document.getElementById("divinho");
+    cor.style.backgroundColor = "black";
+}
+
+function caixinha4 ( ){
+    let texto = document.getElementById("pnovo");
+    texto.textContent = "Achooou!"
+}
+
+function caixinha5 ( ){
+    let texto = document.getElementById("invisivel");
+    texto.textContent = "";
+    let botao = document.getElementById("btn5");
+    botao.classList.add("botaosangue");
+}
