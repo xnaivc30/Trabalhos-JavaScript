e7afc6181ff7a475a471afa592199ec44a4f82e7
xnaivc30 <tricknick30@gmail.com>
2026-05-14 15:28:38 -0300

    Add files via upload

---
+++ aulajs3/script.js
@@ -0,0 +1,76 @@
+function caixinha1(){
+    let oxi1 = document.getElementById("input1").value;
+        if (oxi1 == "") {
+            document.getElementById("p1").textContent = "... Aguardando a idade ...";
+        }
+        else if (oxi1 < 0 ){
+            document.getElementById("p1").textContent = "... Aguardando a idade ...";
+        }
+        else if (oxi1 < 12){
+            document.getElementById("p1").textContent = "Astíanax";
+        }
+        else if (oxi1 >= 12 && oxi1 <= 17){
+            document.getElementById("p1").textContent = "Telêmaco";
+        }
+        else if (oxi1 >= 18 && oxi1 <= 59){
+            document.getElementById("p1").textContent = "Odisseu";
+        }
+        else if (oxi1 >= 60){
+            document.getElementById("p1").textContent = "Anticléia";
+        } 
+}
+
+function caixinha2(){
+    let oxi2 = document.getElementById("input2").value;
+    let oxi3 = document.getElementById("input3").value;
+        if (oxi2 == "Odisseu" && oxi3 == "penelope123"){
+            alert ("Bem vindo, Odisseu")
+        }
+        else if (oxi2 == "Ninguém" && oxi3 == "vinho"){
+            alert ("Modo anônimo ativado")
+        }
+        else if (oxi2 == "" || oxi3 == ""){
+            alert ("Preencha todos os campos")
+        }
+        else{
+            alert ("Usuário ou senha incorretos")
+        }
+}
+
+function caixinha3(){
+    let oxi4 = document.getElementById("input4").value;
+    let radio1 = document.getElementById("input5").checked;
+    let radio2 = document.getElementById("input6").checked;
+    let radio3 = document.getElementById("input7").checked;
+    let radio4 = document.getElementById("input8").checked;
+        if (oxi4 >= 500 && radio1 == true && radio2 == false){
+            document.getElementById("h3_desconto").textContent = oxi4 * 0.20;
+        }
+        else if (oxi4 >= 300 && radio3 == true && radio4 == false){
+            document.getElementById("h3_desconto").textContent = oxi4 * 0.10;
+        }
+        else if (oxi4 < 300 && radio3 == false && radio4 == true){
+            document.getElementById("h3_desconto").textContent = "Desconto nenhum oxi"
+        }
+        else {
+            document.getElementById("h3_desconto").textContent = "Deu erro ae oh"
+        }
+}
+
+function caixinha4(){
+    let oxi5 = document.getElementById("input9").value;
+    let radio5 = document.getElementById("input10").checked;
+    let radio6 = document.getElementById("input11").checked;
+        if (oxi5 < 300 && radio5 == false && radio6 == true){
+            document.getElementById("h3_avaliacao").textContent = "Não foi culpa do Odisseu"
+        }
+        else if (oxi5 >= 300 && oxi5 < 450 && radio5 == true && radio6 == false){
+            document.getElementById("h3_avaliacao").textContent = "Talvez tenha sido culpa do Odisseu"
+        }
+        else if (oxi5 >= 450 && radio5 == true && radio6 == false){
+            document.getElementById("h3_avaliacao").textContent = "Foi culpa do Odisseu"
+        }
+        else if (oxi5 < 0 || oxi5 > 601){
+            document.getElementById("h3_avaliacao").textContent = "Mintira, troca isso ai"
+        }
+}
